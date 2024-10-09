8dc783ce6a8d5767c453f0bcd09f225f20f35bd7
RobertasJ <robertasj8@gmail.com>
2024-10-09 20:48:27 +0200

    got it working

--- crates/hooks/src/use_animate.rs
+++ crates/hooks/src/use_animate.rs
@@ -21,19 +21,20 @@ use crate::{use_platform, UsePlatform};
 type EasingFunction = fn(f32, f32, f32, f32) -> f32;
 pub trait Easable {
     type Output;
-    fn ease(&self, to: &Self, time: u32, duration: u32, function: EasingFunction) -> Self::Output;
+    fn ease(self, to: Self, time: u32, duration: u32, function: EasingFunction) -> Self::Output;
 }
 
 impl Easable for f32 {
     type Output = Self;
-    fn ease(&self, to: &Self, time: u32, duration: u32, function: EasingFunction) -> Self::Output {
-        function(time as f32, *self, *to - *self, duration as f32)
+    fn ease(self, to: Self, time: u32, duration: u32, function: EasingFunction) -> Self::Output {
+        function(time as f32, self, to - self, duration as f32)
     }
 }
 
 impl Easable for Color {
     type Output = Self;
-    fn ease(&self, to: &Self, time: u32, duration: u32, function: EasingFunction) -> Self::Output {
+    fn ease(self, to: Self, time: u32, duration: u32, function: EasingFunction) -> Self::Output {
+        println!("{self:#?}\n\n{to:#?}");
         let hsv1 = self.to_hsv();
         let hsv2 = to.to_hsv();
 
@@ -42,7 +43,7 @@ impl Easable for Color {
         let v = function(time as f32, hsv1.v, hsv2.v - hsv1.v, duration as f32);
 
         let eased = HSV { h, s, v };
-        let color = eased.to_color(1);
+        let color = eased.to_color(255);
 
         color
     }
@@ -50,9 +51,9 @@ impl Easable for Color {
 
 impl Easable for &str {
     type Output = String;
-    fn ease(&self, to: &Self, time: u32, duration: u32, function: EasingFunction) -> Self::Output {
-        let color = &(Color::parse(self)).expect("to be a color").ease(
-            &Color::parse(to).unwrap(),
+    fn ease(self, to: Self, time: u32, duration: u32, function: EasingFunction) -> Self::Output {
+        let color = Color::parse(self).expect("to be a color").ease(
+            Color::parse(to).expect("to be a color"),
             time,
             duration,
             function,
@@ -136,14 +137,13 @@ impl<T: Easable<Output = O> + Clone, O: Clone> AnimatedValue for SegmentComposit
     }
 
     fn calc(&self, index: u32) -> Self::Output {
-        println!("{index}");
         let mut accumulated_time = 0;
         let mut res = None;
         for segment in &self.segments {
             if index >= accumulated_time && index <= accumulated_time + segment.duration {
                 let relative_time = index - accumulated_time;
-                res = Some(segment.start.ease(
-                    &segment.end,
+                res = Some(segment.start.clone().ease(
+                    segment.end.clone(),
                     relative_time,
                     segment.duration,
                     segment.function,
@@ -165,14 +165,20 @@ pub trait AnimatedValue {
     fn calc(&self, index: u32) -> Self::Output;
 }
 
-#[derive(PartialEq, Eq, Clone)]
+#[derive(PartialEq, Eq, Clone, Copy)]
 pub struct Context {
     auto_start: bool,
+    starting_direction: Direction,
 }
 
 impl Context {
-    pub fn auto_start(&mut self, auto_start: bool) -> &mut Self {
-        self.auto_start = auto_start;
+    pub fn auto_start(&mut self) -> &mut Self {
+        self.auto_start = true;
+        self
+    }
+
+    pub fn starting_direction(&mut self, direction: Direction) -> &mut Self {
+        self.starting_direction = direction;
         self
     }
 }
@@ -183,7 +189,11 @@ pub enum Direction {
     Backward,
 }
 
-pub struct UseAnimator<O: 'static, Animated: AnimatedValue<Output = O> + PartialEq + 'static> {
+#[derive(Clone, Copy)]
+pub struct UseAnimator<
+    O: 'static + Clone,
+    Animated: AnimatedValue<Output = O> + PartialEq + Clone + 'static,
+> {
     function_and_ctx: Memo<(Animated, Context)>,
     is_running: Signal<bool>,
     task: Signal<Option<Task>>,
@@ -192,15 +202,13 @@ pub struct UseAnimator<O: 'static, Animated: AnimatedValue<Output = O> + Partial
     value: Signal<O>,
 }
 
-impl<O: 'static + Debug, Animated: AnimatedValue<Output = O> + PartialEq + 'static>
+impl<O: 'static + Clone, Animated: AnimatedValue<Output = O> + Clone + PartialEq + 'static>
     UseAnimator<O, Animated>
 {
     pub fn run(&mut self, direction: Direction) {
         *self.direction.write() = direction;
 
         if !(self.is_running)() {
-            println!("Starting the run function...");
-
             let direction = self.direction;
             let function_and_ctx = self.function_and_ctx;
             let mut value = self.value;
@@ -208,31 +216,20 @@ impl<O: 'static + Debug, Animated: AnimatedValue<Output = O> + PartialEq + 'stat
             let platform = self.platform;
             let mut ticker = platform.new_ticker();
 
-            println!("Initial direction: {:?}", direction);
-            println!("Initial value: {:?}", value.read());
-            println!("Is running initially: {:?}", is_running());
-
+            is_running.set(true);
             let task = spawn(async move {
+                platform.request_animation_frame();
                 let mut anchor = match *direction.peek() {
-                    Direction::Forward => {
-                        println!("Direction is Forward, setting anchor to 0");
-                        0
-                    }
+                    Direction::Forward => 0,
                     Direction::Backward => {
                         let duration = function_and_ctx.read().0.duration();
-                        println!(
-                            "Direction is Backward, setting anchor to duration: {:?}",
-                            duration
-                        );
                         duration
                     }
                 };
 
                 let mut offset = Instant::now();
-                println!("Offset time initialized: {:?}", offset);
 
                 let mut last_direction = *direction.peek();
-                println!("Last direction set to: {:?}", last_direction);
 
                 loop {
                     fn offset_time(
@@ -243,104 +240,85 @@ impl<O: 'static + Debug, Animated: AnimatedValue<Output = O> + PartialEq + 'stat
                         match direction {
                             Direction::Forward => {
                                 let elapsed = offset.elapsed().as_millis() as u32;
-                                println!("Direction is Forward, calculating new offset time: anchor + elapsed = {} + {}", anchor, elapsed);
                                 Some(anchor + elapsed)
                             }
                             Direction::Backward => {
                                 let elapsed = offset.elapsed().as_millis() as u32;
-                                println!("Direction is Backward, calculating new offset time: anchor - elapsed = {} - {}", anchor, elapsed);
                                 anchor.checked_sub(elapsed)
                             }
                         }
                     }
 
                     ticker.tick().await;
-                    println!("Ticker ticked...");
 
                     platform.request_animation_frame();
-                    println!("Requested animation frame");
 
                     let current_offset_time =
-                        offset_time(*direction.peek(), anchor, offset).unwrap_or(0);
-                    println!("Current offset time: {:?}", current_offset_time);
+                        offset_time(last_direction, anchor, offset).unwrap_or(0);
 
                     if current_offset_time == 0 {
-                        println!("Offset time is zero, calculating final value...");
-                        *value.write() = function_and_ctx
-                            .read()
-                            .0
-                            .calc(function_and_ctx.read().0.duration());
+                        *value.write() = function_and_ctx.read().0.calc(0);
 
-                        println!("Final value set to: {:?}", *value.write());
                         *is_running.write() = false;
-                        println!("Stopping as value is zero...");
-                    }
-
-                    if current_offset_time >= function_and_ctx.read().0.duration() {
-                        println!("Offset time >= duration, stopping...");
-                        *value.write() = function_and_ctx
-                            .read()
-                            .0
-                            .calc(function_and_ctx.read().0.duration());
-
-                        println!("Final value set to: {:?}", *value.write());
-                        *is_running.write() = false;
-                        println!("Setting is_running to false and exiting...");
-                    }
-
-                    if !is_running() {
-                        println!("Not running anymore, breaking out of loop...");
                         break;
                     }
 
+                    if current_offset_time >= function_and_ctx.read().0.duration() {
+                        *value.write() = function_and_ctx
+                            .read()
+                            .0
+                            .calc(function_and_ctx.read().0.duration());
+
+                        *is_running.write() = false;
+                        break;
+                    }
+
+                    if !is_running() {}
+
                     if last_direction != *direction.peek() {
-                        println!(
-                            "Direction has changed! Last direction: {:?}, New direction: {:?}",
-                            last_direction,
-                            *direction.peek()
-                        );
+                        println!("direction changed");
                         anchor =
                             offset_time(last_direction, anchor, offset).expect("to not underflow");
-                        println!("New anchor calculated after direction change: {:?}", anchor);
                         offset = Instant::now();
-                        println!("Resetting offset to now: {:?}", offset);
+                        last_direction = *direction.peek();
                     }
 
                     *value.write() = function_and_ctx.read().0.calc(
                         offset_time(*direction.peek(), anchor, offset).expect("to not underflow"),
                     );
-                    println!("Updated value: {:?}", *value.write());
                 }
             });
 
-            println!("Spawning task...");
             let mut x: Write<Option<Task>, UnsyncStorage> = self.task.write();
             x.replace(task);
 
-            println!("Task replaced, returning from run function.");
             return;
-        } else {
-            println!("Already running, exiting early.");
         }
     }
 
-    pub fn value(&self) -> Signal<O> {
-        self.value
+    pub fn value(&self) -> ReadOnlySignal<O> {
+        ReadOnlySignal::new(self.value)
     }
 }
 
-pub fn use_animation<O: 'static, Animated: AnimatedValue<Output = O> + PartialEq + 'static>(
+pub fn use_animation<
+    O: 'static + Clone,
+    Animated: AnimatedValue<Output = O> + Clone + PartialEq + 'static,
+>(
     run: impl Fn(&mut Context) -> Animated + 'static,
 ) -> UseAnimator<O, Animated> {
     let function_and_ctx = use_memo(move || {
-        let mut ctx = Context { auto_start: false };
+        let mut ctx = Context {
+            auto_start: false,
+            starting_direction: Direction::Forward,
+        };
         (run(&mut ctx), ctx)
     });
 
     let task = use_signal(|| None);
     let platform = use_platform();
-    let is_running = use_signal(move || function_and_ctx.read().1.auto_start);
-    let direction = use_signal(move || Direction::Forward);
+    let is_running = use_signal(move || false);
+    let direction = use_signal(move || function_and_ctx.read().1.starting_direction);
     let value = use_signal(move || {
         let time = match *direction.peek() {
             Direction::Forward => 0,
@@ -349,14 +327,21 @@ pub fn use_animation<O: 'static, Animated: AnimatedValue<Output = O> + PartialEq
         function_and_ctx.read().0.calc(time)
     });
 
-    let mut animator = UseAnimator {
-        function_and_ctx,
-        is_running,
-        direction,
-        platform,
-        task,
-        value,
-    };
+    use_hook(move || {
+        let mut animator = UseAnimator {
+            function_and_ctx,
+            is_running,
+            direction,
+            platform,
+            task,
+            value,
+        };
+        let ctx = animator.function_and_ctx.read().1;
 
-    animator
+        if ctx.auto_start {
+            animator.run(ctx.starting_direction);
+        }
+
+        animator
+    })
 }
